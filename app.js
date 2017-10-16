20d45c5bcd486959bab151171110788c819e961a
Eric P. Katz <ericpkatz@gmail.com>
2017-10-16 17:16:15 -0400

    commit

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const ejs = require('ejs');
 
 const app = express();
+let config = process.env;
+try {
+  config = require('./env');
+}
+catch(ex){
+ 
+}
+
 app.use(session({
-  secret: 'foo'
+  secret: config.SESSION_SECRET
 }));
 
 module.exports = app;
@@ -14,6 +23,10 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+app.set('views', path.join(__dirname, '/public'));
+
 app.use(require('body-parser').json());
 
 [
@@ -22,7 +35,9 @@ app.use(require('body-parser').json());
   '/cart',
   '/cart/address',
   '/orders'
-].map( url => app.get(url, (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html'))));
+].forEach( url => app.get(url, (req, res, next)=> {
+  res.render('index.html', { GOOGLE_PLACES_API_KEY: config.GOOGLE_PLACES_API_KEY });
+}));
 
 app.use('/api', require('./api'));
 
